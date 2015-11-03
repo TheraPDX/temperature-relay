ec75bf03f4e524fa2e9739e94f3d2df2d07369b2
Jeffery Utter <jeff@jeffutter.com>
2015-11-03 08:21:47 -0600

    Add metrics webserver

--- temperature-relay.ino
+++ temperature-relay.ino
@@ -1,4 +1,5 @@
 #include "OneWire.h"
+#include "WebServer.h"
 #include "elapsedMillis.h"
 #include <list>
 #include "Sensors.h"
@@ -9,6 +10,11 @@ using namespace std;
 #define TEMP_INTERVAL 10000
 #define BLINK_INTERVAL 5000
 #define POWER_INTERVAL 120000
+#define PREFIX ""
+
+template<class T>
+inline Print &operator <<(Print &obj, T arg)
+{ obj.print(arg); return obj; }
 
 elapsedMillis scanTimeElapsed;
 elapsedMillis tempTimeElapsed;
@@ -17,6 +23,7 @@ elapsedMillis powerTimeElapsed;
 
 OneWire ds(D1);
 Sensors sensors(ds);
+WebServer webserver(PREFIX, 80);
 
 int led1 = D7;
 int powertail = D5;
@@ -34,6 +41,29 @@ void startup() {
 
 STARTUP( startup() );
 
+void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool){
+  /* server.httpSuccess(); */
+  server.httpSuccess("text/plain; version=0.0.4");
+  if (type != WebServer::HEAD) {
+    /* P(helloMsg) = "<h1>Hello, World!</h1>"; */
+    char s_temp[75];
+    snprintf(s_temp, 75,"# TYPE temp_degrees gauge\ntemp_degrees %.2f\n\n", temperature);
+
+    char s_average_temp[100];
+    snprintf(s_average_temp, 100, "# TYPE average_temp_degrees gauge\naverage_temp_degrees %.2f\n\n", minuteAverage);
+
+    char s_power[30];
+    snprintf(s_power, 30, "# TYPE heater gauge\nheater %i\n\n", power);
+
+    /* server.print(stats); */
+
+    server << s_temp;
+    server << s_average_temp;
+    server << s_power;
+  }
+}
+
+
 void setup(void) {
   Serial.begin(57600);
   
@@ -43,6 +73,10 @@ void setup(void) {
   Particle.variable("threshold", tempThreshold);
   Particle.function("power", adjustPower);
   publishPowerStatus();
+
+  webserver.setDefaultCommand(&helloCmd);
+  webserver.addCommand("metrics", &helloCmd);
+  webserver.begin();
 
   sensors.scan();
   sensors.debug();
@@ -106,6 +140,10 @@ void debugPublish(const char message[]) {
 }
 
 void loop(void) {
+  char buff[64];
+  int len = 64;
+  webserver.processConnection(buff, &len);
+
   if (blinkTimeElapsed > BLINK_INTERVAL) {
     ledState = !ledState; 
     digitalWrite(led1, ledState);
@@ -127,10 +165,16 @@ void loop(void) {
     Serial.println("## Reading Temps");
     sensors.read();
 
+    minuteAverage = sensors.minute_average;
+    temperature = sensors.temp;
+
+    Serial.print("Temp: ");
+    Serial.println(temperature);
     Serial.print("Average Temp: ");
-    Serial.println(sensors.minute_average);
-    publishAverage(sensors.minute_average);
-    publishTemp(sensors.temp);
+    Serial.println(minuteAverage);
+
+    publishAverage(minuteAverage);
+    publishTemp(temperature);
   }
 
   if (powerTimeElapsed > POWER_INTERVAL) {
@@ -138,7 +182,7 @@ void loop(void) {
 
     Serial.println("## Evaluating Power");
 
-    if(sensors.minute_average > tempThreshold) {
+    if(minuteAverage > tempThreshold) {
       turnOffPower();
     } else {
       turnOnPower();
