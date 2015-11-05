46594a098a9871935ebb134a8e9044fee03a2232
Jeffery Utter <jeff@jeffutter.com>
2015-11-04 21:35:20 -0600

    Add HTTP Client to fetch local weather conditions

--- temperature-relay.ino
+++ temperature-relay.ino
@@ -1,8 +1,11 @@
 #include "OneWire.h"
 #include "WebServer.h"
+#include "HttpClient.h"
 #include "elapsedMillis.h"
 #include <list>
+#include <string>
 #include "Sensors.h"
+#include "ApiKeys.h"
 
 using namespace std;
 
@@ -10,7 +13,9 @@ using namespace std;
 #define TEMP_INTERVAL 10000
 #define BLINK_INTERVAL 5000
 #define POWER_INTERVAL 120000
+#define WEATHER_INTERVAL 120000
 #define PREFIX ""
+#define WEATHER_ZIP "68522"
 
 template<class T>
 inline Print &operator <<(Print &obj, T arg)
@@ -20,16 +25,27 @@ elapsedMillis scanTimeElapsed;
 elapsedMillis tempTimeElapsed;
 elapsedMillis blinkTimeElapsed;
 elapsedMillis powerTimeElapsed; 
+elapsedMillis weatherTimeElapsed; 
 
 OneWire ds(D1);
 Sensors sensors(ds);
 WebServer webserver(PREFIX, 80);
+HttpClient http;
+
+http_header_t headers[] = {
+  { "User-Agent", "curl/7.43.0"},
+  { "Accept" , "*/*"},
+  { NULL, NULL }
+};
+http_request_t request;
+http_response_t response;
 
 int led1 = D7;
 int powertail = D5;
 
 float temperature;
 float minuteAverage;
+float outdoorTemp;
 float tempThreshold = 62.0F;
 bool power = false;
 boolean ledState = LOW;
@@ -42,11 +58,11 @@ void startup() {
 STARTUP( startup() );
 
 void metricsCmd(WebServer &server, WebServer::ConnectionType type, char *, bool){
-  /* server.httpSuccess(); */
   server.httpSuccess("text/plain; version=0.0.4");
   if (type != WebServer::HEAD) {
-    char s_temp[75];
-    char s_average_temp[75];
+    char s_temp[100];
+    char s_average_temp[100];
+    char s_outdoor_temp[100];
     char s_power[50];
     char s_freemem[110];
     uint32_t freemem;
@@ -54,13 +70,15 @@ void metricsCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
     freemem = System.freeMemory();
 
     server << "# TYPE temp_degrees gauge\n";
-    snprintf(s_temp, 75,"temp_degrees{timespan=\"none\"} %.4f %li000\n", temperature, Time.now());
-    snprintf(s_average_temp, 75, "temp_degrees{timespan=\"minute\"} %.4f %li000\n\n", minuteAverage, Time.now());
+    snprintf(s_temp, 100,"temp_degrees{location=\"garage\",timespan=\"none\"} %.4f %li000\n", temperature, Time.now());
+    snprintf(s_average_temp, 100, "temp_degrees{location=\"garage\",timespan=\"minute\"} %.4f %li000\n", minuteAverage, Time.now());
+    snprintf(s_outdoor_temp, 100, "temp_degrees{location=\"outdoors\",timespan=\"none\"} %.4f %li000\n\n", outdoorTemp, Time.now());
     snprintf(s_power, 50, "# TYPE heater gauge\nheater %i %li000\n\n", power, Time.now());
     snprintf(s_freemem, 110, "# Type free_mem_bytes\nfree_mem_bytes %lu %li000\n\n", freemem, Time.now());
 
     server << s_temp;
     server << s_average_temp;
+    server << s_outdoor_temp;
     server << s_power;
     server << s_freemem;
   }
@@ -137,9 +155,62 @@ void publishAverage(float temp) {
   Serial.println(publishString);
 }
 
+void publishOutdoor(float temp) {
+  char publishString[20];
+  
+  snprintf(publishString, 20,"%.4f", temp);
+  Particle.publish("outdoor_temp", publishString);
+  Serial.print("Outdoor: ");
+  Serial.println(publishString);
+}
+
 void debugPublish(const char message[]) {
   Particle.publish("debug", message);
   Serial.println(message);
+}
+
+String tryExtractString(String str, const char* start, const char* end) {
+  if (str == NULL) {
+    return NULL;
+  }
+
+  int idx = str.indexOf(start);
+  if (idx < 0) {
+    return NULL;
+  }
+
+  int endIdx = str.indexOf(end);
+  if (endIdx < 0) {
+    return NULL;
+  }
+
+  return str.substring(idx + strlen(start), endIdx);
+}
+
+void fetchWeather() {
+  String body;
+  String tempStr;
+
+  Serial.println("Fetching Weather");
+
+  request.hostname = "api.openweathermap.org";
+  request.port = 80;
+  request.path="/data/2.5/weather?zip=" WEATHER_ZIP ",us&units=imperial&appid=" OPENWEATHERMAP_API_KEY;
+  http.get(request, response, headers);
+
+  Particle.publish("Weather HTTP Code", String(response.status));
+  Serial.println(response.status);
+  Serial.println("### HTTP BODY START ###");
+  Serial.println(response.body);
+  Serial.println("### HTTP BODY END ###");
+
+  body = String(response.body);
+  tempStr = tryExtractString(body, "\"main\":{\"temp\":",",\"pressure\":");
+  
+  if (tempStr != NULL) {
+    outdoorTemp = tempStr.toFloat();
+    publishOutdoor(outdoorTemp);
+  }
 }
 
 void loop(void) {
@@ -194,5 +265,10 @@ void loop(void) {
     } else {
       turnOnPower();
     }
+  }
+
+  if (weatherTimeElapsed > WEATHER_INTERVAL) {
+    weatherTimeElapsed = 0;
+    fetchWeather();
   }
 }
